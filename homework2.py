823bc45d43cf06636bf8575beb11e6de1c87cf72
fanflajok <fanflajok@gmail.com>
2024-07-19 00:22:52 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,5 @@
+homework_count=int("12")
+spent_houres=float("1.5")
+name_of_the_course=str("Python")
+time_for_one_task=spent_houres/homework_count
+print("Курс:",(str(name_of_the_course+str(","))),"всего задач:"+str(homework_count)+"," , "затрачено часов:",str(spent_houres)+",","среднее время выполнения",(float((time_for_one_task))),"часа.")
